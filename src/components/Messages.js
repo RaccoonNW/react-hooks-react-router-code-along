7a59afaf82f00423fd8a47fc1a7f778f461eab77
Gabriel <shalitmontagne.g@gmail.com>
2022-01-19 10:59:48 -0700

    Completed Assignment

---
+++ src/components/Messages.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Messages() {
+    return (
+        <div>
+            <ul>
+                <li>Just a reminder to finish this lesson</li>
+                <li>Please move on with the lesson</li>
+                <li>This is the last message</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Messages
